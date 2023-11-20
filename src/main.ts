6b487693e97b05079871021bd773d74b911f8ab6
daskrys <caperezl@ucsc.edu>
2023-11-19 18:13:28 -0800

    added plant class

--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,12 @@
 import * as Phaser from "phaser";
 import Menu from "./scenes/Menu";
+//import PlantTest from "./scenes/PlantTest";
 
 const config: Phaser.Types.Core.GameConfig = {
   width: 640,
   height: 480,
   scene: [Menu],
+  //scene: [PlantTest],
 };
 
 document.title = "CMPM 121- Final Project";
