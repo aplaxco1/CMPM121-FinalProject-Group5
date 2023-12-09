aa38d965221adbdc22e3ce98c73b6867c704b62e
daskrys <caperezl@ucsc.edu>
2023-12-09 15:18:57 -0800

    config comment for capjs

--- vite.config.js
+++ vite.config.js
@@ -1,4 +1,12 @@
 export default {
   // eslint-disable-next-line no-undef
-  base: process.env.REPO_NAME || "/repo-name/",
+  base: process.env.REPO_NAME || "/repo-name/", // comment out for Capacitor
+  // uncomment for Capacitor
+  /*
+  base: process.env.REPO_NAME || "/",
+  
+  build: {
+    chunkSizeWarningLimit: 2000,
+  },
+  */ 
 };
